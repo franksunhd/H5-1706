94aed2874722a9bddd4f24e69ba02a9b7c695cc6
franksun <sun_si_yan@163.com>
2017-08-12 16:24:34 +0800

    Add a ifa.c file
    Add a ifa.c file

---
+++ ifa.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void){
+
+    int a=-1;
+
+    if(a>0)
+    {
+        printf("a>0\n");
+    }
+    if(a<0)
+    {
+        printf("a<0\n");
+    }
+    if(a==0)
+    {
+        printf("a==0\n");
+    }
+
+    printf("a=%d\n",a);
+    return 0;
+}
