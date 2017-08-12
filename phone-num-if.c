b94de4f434cca483e7826c62d59aae3c04c5e6b7
franksun <sun_si_yan@163.com>
2017-08-12 15:59:33 +0800

    if else方法输出数字号码盘
    if else方法输出数字号码盘

---
+++ phone-num-if.c
@@ -0,0 +1,36 @@
+//使用if(){}else{}的电话号码盘转换
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+    int ch;
+    while((ch=getchar())!=EOF){
+        if(isalpha(ch))
+        {
+            ch=toupper(ch);
+            if(ch=='A'||ch=='B'||ch=='C')
+                putchar('2');
+            else if(ch=='D'||ch=='E'||ch=='F')
+                putchar('3');
+            else if(ch=='G'||ch=='H'||ch=='I')
+                putchar('4');
+            else if(ch=='J'||ch=='K'||ch=='L')
+                putchar('5');
+            else if(ch=='M'||ch=='N'||ch=='O')
+                putchar('6');
+            else if(ch=='P'||ch=='Q'||ch=='R'||ch=='S')
+                putchar('7');
+            else if(ch=='T'||ch=='U'||ch=='V')
+                putchar('8');
+            else if(ch=='W'||ch=='X'||ch=='Y'||ch=='Z')
+                putchar('9');
+        }
+        else
+        {
+            putchar(ch);
+        }
+
+    }
+    return 0;
+}
